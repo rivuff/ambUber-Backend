c33ca649d48c92a87a57a62c7ec145211b0cc43e
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2023-06-06 10:08:44 +0530

    bhumi pujo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
+const connectDb = require('./config/dbConfig');
+const app = express();
+connectDb();
+const setupAndStartServer = async() => {
 
-const setupAndStartServer = async ()=>{
-    const app = express();
-
-    app.listen(PORT, async()=>{
+    app.listen(PORT, async() => {
         console.log(`server started at port ${PORT}`);
     });
 }
