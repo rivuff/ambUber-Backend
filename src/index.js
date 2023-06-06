85e61e2acf7aefe16123ebff090f7a676a7905a3
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2023-06-06 10:29:18 +0530

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const connectDb = require('./config/dbConfig');
 const app = express();
 connectDb();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
